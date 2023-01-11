a6f87b01e3c8d4290a51c1f9f0bd7fe1f6a29634
Matthew Gibbons <matthew@southwestkoala.co.uk>
2023-01-11 11:00:45 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use clap::Parser;
+use reqwest::{Client, Url};
+use tokio::task::JoinSet;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version)]
+struct Args {
+    /// URLs to check
+    #[arg(required = true)]
+    urls: Vec<Url>,
+}
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
+async fn main() {
+    let args = Args::parse();
+    let mut set = JoinSet::new();
+
+    for url in args.urls {
+        set.spawn(async move {
+            let builder = Client::builder().user_agent("healthcheck".to_string());
+
+            if let Ok(client) = builder.build() {
+                match client.head(url.to_string()).send().await {
+                    Ok(_) => println!("🟢 {}", url),
+                    Err(err) => println!("🔴 {} ({})", url, err.without_url()),
+                }
+            };
+        });
+    }
+
+    while let Some(_) = set.join_next().await {}
+}
